6492f9daa85ce8e5b236a38e7338faf714e0edaa
Arash <arash@probe.dk>
2024-10-09 10:37:53 +0200

    create invite-employee button

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SidebarLayout } from "@/components/SidebarLayout";
+import CustomButton from "@/components/CustomButton";
 import EmployeesListForm from "@/components/EmployeesListForm";
 import useGetEmployeesInfo from "@/hooks/useGetEmployeesInfo";
 import useLanguageStore from "@/stores/language-store";
@@ -39,9 +40,18 @@ export default function CompanyPage() {
 
   return (
     <SidebarLayout>
-      <h1 className="text-2xl font-semibold mt-3 mb-8 sm:text-3xl sm:mb-12">
-        {l("settings.title") || "Employees List"}
-      </h1>
+      <div className="flex flex-col justify-between md:flex-row md:items-center">
+        <h1 className="text-2xl font-semibold mt-3 mb-8 sm:text-3xl sm:mb-12">
+          {l("settings.title") || "Employees List"}
+        </h1>
+        <div className="flex justify-center xs:justify-end gap-4 xl:items-end pb-4 xs:pb-8">
+          <CustomButton
+            title={l("settings.form.save") || "Invite Employee"}
+            containerStyles="rounded-lg h-[48px] gradient-green1 hover1"
+            btnType="button"
+          />
+        </div>
+      </div>
       {dummyEmployees.map((employee, index) => (
         <div
           key={index}
