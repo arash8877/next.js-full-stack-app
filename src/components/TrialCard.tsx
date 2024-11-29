fc89b0b88cf43dce4315bf21551da125069e9369
Arash <arash@probe.dk>
2024-11-29 12:37:28 +0100

    fixed applicationCount in the card

--- src/components/TrialCard.tsx
+++ src/components/TrialCard.tsx
@@ -94,17 +94,19 @@ iTrialCardProps) {
             title={
               <div className="flex items-center justify-between w-full">
                 <span>{l("trialcard.cta.text") || "Applicants"}</span>
-                {applicationCount > 0 && (
-                  <span className="ml-2 bg-red-400 text-white text-xs font-bold px-2 py-1 rounded-full">
-                    {applicationCount}
-                  </span>
-                )}
+                {applicationCount !== null &&
+                  applicationCount !== undefined && (
+                    <span className="ml-2 bg-red-400 text-white text-xs font-bold w-6 h-6 flex items-center justify-center rounded-full">
+                      {applicationCount}
+                    </span>
+                  )}
               </div>
             }
             containerStyles="rounded-lg gradient-green1 mt-4 hover1 custom-width-btn"
             btnType="button"
           />
         </Link>
+
         <div className="flex flex-col">
           <Link href={`/trials/${trialId}`}>
             <CustomButton
