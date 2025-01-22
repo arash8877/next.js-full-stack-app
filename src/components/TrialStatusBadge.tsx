8e180ce331b3be1523f5b77ddc098cf28c9251ef
Arash <arash@probe.dk>
2025-01-22 10:58:42 +0100

    trial cart status badge is fixed

--- src/components/TrialStatusBadge.tsx
+++ src/components/TrialStatusBadge.tsx
@@ -8,29 +8,28 @@ interface iTrialStatusBadgeProps {
 //----------------------------- main function -------------------------------
 export default function TrialStatusBadge({
   approvedAt,
-  publishedAt,
   referred,
   declined,
+  // publishedAt,
 }: iTrialStatusBadgeProps) {
+
+
   //--- getStatusText function ---
   const getStatusText = () => {
-    if (
-      approvedAt === "0001-01-01T00:00:00" &&
-      publishedAt === "0001-01-01T00:00:00"
-    )return "Pending Approval";
-    if (approvedAt !== "0001-01-01T00:00:00") return "Approved";
     if (declined) return "Declined";
     if (referred) return "Referred";
+    if (approvedAt === "0001-01-01T00:00:00") return "Pending Approval";
+    if (approvedAt !== "0001-01-01T00:00:00") return "Approved";
     // if (publishedAt !== "0001-01-01T00:00:00") return "Published";
   };
 
   const getBgColor = () => {
     const status = getStatusText();
-    // if (status === "Published") return "bg-blue-500 text-white";
-    if (status === "Approved") return "bg-bgColor-green2 text-black";
     if (status === "Pending Approval") return "bg-bgColor-yellow2 text-black";
+    if (status === "Approved") return "bg-bgColor-green2 text-white";
     if (status === "Referred") return "bg-bgColor-blue text-black";
-    if (status === "Declined") return "bg-bgColor-red2 text-black";
+    if (status === "Declined") return "bg-bgColor-red2 text-white";
+    // if (status === "Published") return "bg-blue-500 text-white";
     return "bg-transparent";
   };
 
