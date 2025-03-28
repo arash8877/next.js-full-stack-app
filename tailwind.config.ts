dd1e1cee7d09ec2dd44ece312380b038d33b83c5
Arash <arash@probe.dk>
2025-03-28 15:18:03 +0100

    modify tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,20 @@
-import daisyui from 'daisyui';
-
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, 
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF5722',
+      }
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [daisyui],
+  plugins: [],
 }
