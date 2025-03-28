489907ea0bf70ed1b5b2e69c91c2b91df56d63fd
Arash <arash@probe.dk>
2025-03-28 15:05:29 +0100

    fix tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import daisyui from 'daisyui';
+
+
+module.exports = {
+  purge: [],
+  darkMode: false, 
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
