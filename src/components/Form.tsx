dab8e43cf254fdc5aa150c21f46bbe913bdb9fff
Arash <arash8877@gmail.com>
2025-05-12 22:12:54 +0200

    create Form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,7 +8,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit } : FormTypes) => 
         <span className='blue_gradient'>{type} Post</span>
       </h1>
       <p className='desc text-left max-w-md'>
-        {type} and share amazing prompts with the world, and let your
+        Share amazing prompts with the world, and let your
         imagination run wild with any AI-powered platform
       </p>
 
