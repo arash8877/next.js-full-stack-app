4610a02d82b535d1f63b89ea66eae936df9b8e83
Arash <arash8877@gmail.com>
2025-05-13 10:02:29 +0200

    create create-prompt page

--- src/app/create-prompt/page.tsx
+++ src/app/create-prompt/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
+
+
+import type { FormEvent } from "react";
+import Form from "@/components/Form";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const CreatePrompt = () => {
-  return (
-    <div>CreatePrompt</div>
-  )
-}
+  const router = useRouter();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
-export default CreatePrompt
+  const createPrompt = async (e: FormEvent<Element>) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
